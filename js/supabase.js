688ea9fb240dd40e7f9e6039e2f81924d0a295ed
pereira <rafaellsanttos002@gmail.com>
2026-05-27 15:25:13 +0100

    first sets

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ijswdmvksmqldhkyghwr.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_rFHZWIWlbDIuADRyvl3S4w_mWrelj1j";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
